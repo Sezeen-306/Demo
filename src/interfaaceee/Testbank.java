35b9d74e4bbaec31e29aef99e3ef779377b313fa
sayee <sezeen306305@gmail.com>
2022-04-26 19:15:04 -0400

    migrate fromeclipse to intellij

---
+++ src/interfaaceee/Testbank.java
@@ -0,0 +1,29 @@
+package interfaaceee;
+
+public class Testbank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//	Usbank.i=78;
+		
+		System.out.println(Usbank.i);
+		
+
+		
+		HSBC hs= new HSBC();
+		hs.cash();
+		hs.credit();
+		hs.debit();
+		
+		System.out.println("dynamic polymorphism");
+		
+		Usbank us=new HSBC();
+		us.cash();
+	    //us.debit();
+		
+		
+		
+	}
+
+}
